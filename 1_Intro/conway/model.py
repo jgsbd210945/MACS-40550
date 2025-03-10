a8d38aa7cdb0be4ef80ba2f4acc69144b75607d2
dapeterson-plsc <dapeterson@uchicago.edu>
2025-03-10 12:18:36 -0500

    Update model.py

--- 1_Intro/conway/model.py
+++ 1_Intro/conway/model.py
@@ -1,59 +1,20 @@
-from mesa import Model, Agent
-from mesa.time import RandomActivation
+from mesa import Model
+from agents import ConwayAgent
 from mesa.space import SingleGrid
 
-from agents import ConwayAgent
-import random 
- 
 class ConwayModel(Model):
-    """
-    Model class for the Conway GOL model: sets up agents advances each time step. 
-    """
-    
-    def __init__(self, height, width, num_alive):
-        """ 
-        Add agents to the grid
-        """
-        self.height = height
-        self.width = width
-        self.num_alive = num_alive
-        self.schedule = RandomActivation(self)
-        self.grid = SingleGrid(width, height, torus = False)
-        
-        # working with random seed
-        seed = 10 # can replace 10 with random.random()
-        Model.reset_randomizer(self, seed) #comment this out -- helps for replicability
-        random.seed(seed)
-        #print(f"{self._seed=}")
-        #print(num_alive["value"])
-
-        # Set up agents
-        k = 0
-        for i in range(height):
-            for j in range (width):
-                k = k + 1
-                
-                if k <= num_alive:
-                    status = 1
-                     
-                else:
-                    status = 0
-
-                agent = ConwayAgent((i, j), self, status)
-                #self.grid.place_agent(agent, self.grid.find_empty() ) #notice what this can do! 
-                self.grid.place_agent(agent, (i,j))
-                #print("x: ", str(i), "y: ", str(j), "  status: ", str(status), str(agent.pos))
-                self.schedule.add(agent)
-
-        
-
- 
+    def __init__(self, width = 100, height = 100, start_alive = 0.3, seed = None):
+        super().__init__(seed = seed)
+        self.grid = SingleGrid(width, height, torus = True)
+        for cont, (x, y) in self.grid.coord_iter():
+            conway = ConwayAgent(self, (x, y))
+            if self.random.random() < start_alive:
+                conway.state = 1
+            else:
+                conway.state = 0
+            self.grid.place_agent(conway, (x, y))
+        self.running = True
 
     def step(self):
-        """
-        Run one step of the model. 
-        """
-        self.schedule.step()
-
-
-
+        self.agents.do("determine_next_state")
+        self.agents.do("live_or_die") 
