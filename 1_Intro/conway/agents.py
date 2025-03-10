33501fd9af4f1439c7960c85360fbb5005905afa
dapeterson-plsc <dapeterson@uchicago.edu>
2025-03-10 12:18:59 -0500

    Update agents.py

--- 1_Intro/conway/agents.py
+++ 1_Intro/conway/agents.py
@@ -1,42 +1,21 @@
-from mesa import Model, Agent
-from mesa.space import SingleGrid
-from mesa.visualization import TextVisualization
+from mesa import Agent
 
 class ConwayAgent(Agent):
-    """
-    Conway automata: creates agents and rules for life. 
-    """
-
-    def __init__(self, pos, model, status):
-        """
-        Create a new automata.
-
-        Args:
-            pos: where the agent is
-           status: Indicator for the agent's type (alive=1, dead=0)
-        """
-        super().__init__(pos, model) # allows base class to work with pos + model
-        self.pos = pos
-        self.status = status
-
-
-    def step(self):
-        living_neighbors = 0
-        for neighbor in self.model.grid.iter_neighbors(self.pos, True):
-            if neighbor.status == 1:
-                living_neighbors += 1
-
-
-        # determine neighborhood crowding; die or come alive!
-        if self.status == 0 and living_neighbors == 3:
-            self.status = 1 #brought to life by living neighbors!
-
-        elif self.status == 1:  # you're alive
-            if living_neighbors < 2: 
-                self.status = 0 #underpopulated
-            elif living_neighbors > 3:
-                self.status = 0 #overpopulated
+    def __init__(self, model, pos, state = 0):
+        super().__init__(model)
+        self.x, self.y = pos
+        self.state = None
+        self.new_state = None
+    def determine_next_state(self):
+        live_neighbors = sum(neighbor.state for neighbor in self.model.grid.iter_neighbors((self.x, self.y), True))
+        if self.state == 1:
+            if live_neighbors < 2 or live_neighbors > 3:
+                self.new_state = 0
+            else: self.new_state = 1
+        else:
+            if live_neighbors == 3:
+                self.new_state = 1
             else:
-                self.status = 1 #juuuust right
-
-
+                self.new_state = 0
+    def live_or_die(self):
+        self.state = self.new_state
