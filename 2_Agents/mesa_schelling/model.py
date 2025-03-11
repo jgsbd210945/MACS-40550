895e286e4fedd546e7cf0b7b272dbe27e06961a3
dapeterson-plsc <dapeterson@uchicago.edu>
2025-03-11 12:55:21 -0500

    update schelling

--- 2_Agents/mesa_schelling/model.py
+++ 2_Agents/mesa_schelling/model.py
@@ -1,102 +1,47 @@
-import mesa
+from mesa import Model
+from mesa.space import SingleGrid
+from agents import SchellingAgent
+from mesa.datacollection import DataCollector
 
-
-class SchellingAgent(mesa.Agent):
-    """
-    Schelling segregation agent
-    """
-
-    def __init__(self, unique_id, model, agent_type):
-        """
-        Create a new Schelling agent.
-
-        Args:
-           unique_id: Unique identifier for the agent.
-           x, y: Agent initial location.
-           agent_type: Indicator for the agent's type (minority=1, majority=0)
-        """
-        super().__init__(unique_id, model)
-        self.type = agent_type
-
-    def step(self):
-        similar = 0
-        for neighbor in self.model.grid.iter_neighbors(
-            self.pos, moore=True, radius=self.model.radius
-        ):
-            if neighbor.type == self.type:
-                similar += 1
-
-        # If unhappy, move:
-        if similar < self.model.homophily:
-            self.model.grid.move_to_empty(self)
-        else:
-            self.model.happy += 1
-
-
-class Schelling(mesa.Model):
-    """
-    Model class for the Schelling segregation model.
-    """
-
-    def __init__(
-        self,
-        height=20,
-        width=20,
-        homophily=3,
-        radius=1,
-        density=0.8,
-        minority_pc=0.2,
-        seed=None,
-    ):
-        """
-        Create a new Schelling model.
-
-        Args:
-            width, height: Size of the space.
-            density: Initial Chance for a cell to populated
-            minority_pc: Chances for an agent to be in minority class
-            homophily: Minimum number of agents of same class needed to be happy
-            radius: Search radius for checking similarity
-            seed: Seed for Reproducibility
-        """
-
+class SchellingModel(Model):
+    ## Define initiation, requiring all needed parameter inputs
+    def __init__(self, width = 50, height = 50, density = 0.7, desired_share_alike = 0.5, group_one_share = 0.7, radius = 1, seed = None):
+        ## Inherit seed trait from parent class
         super().__init__(seed=seed)
-        self.height = height
+        ## Define parameter values for model instance
         self.width = width
+        self.height = height
         self.density = density
-        self.minority_pc = minority_pc
-        self.homophily = homophily
+        self.desired_share_alike = desired_share_alike
+        self.group_one_share = group_one_share
         self.radius = radius
-
-        self.schedule = mesa.time.RandomActivation(self)
-        self.grid = mesa.space.SingleGrid(width, height, torus=True)
-
+        ## Create grid
+        self.grid = SingleGrid(width, height, torus = True)
+        ## Instantiate global happiness tracker
         self.happy = 0
-        self.datacollector = mesa.DataCollector(
-            model_reporters={"happy": "happy"},  # Model-level count of happy agents
+        ## Define data collector, to collect happy agents and share of agents currently happy
+        self.datacollector = DataCollector(
+            model_reporters = {
+                "happy" : "happy",
+                "share_happy" : lambda m : (m.happy / len(m.agents)) * 100
+                if len(m.agents) > 0
+                else 0
+            }
         )
-
-        # Set up agents
-        # We use a grid iterator that returns
-        # the coordinates of a cell as well as
-        # its contents. (coord_iter)
-        for _, pos in self.grid.coord_iter():
+        ## Place agents randomly around the grid, randomly assigning them to agent types.
+        for cont, pos in self.grid.coord_iter():
             if self.random.random() < self.density:
-                agent_type = 1 if self.random.random() < self.minority_pc else 0
-                agent = SchellingAgent(self.next_id(), self, agent_type)
-                self.grid.place_agent(agent, pos)
-                self.schedule.add(agent)
-
+                if self.random.random() < self.group_one_share:
+                    self.grid.place_agent(SchellingAgent(self, 1), pos)
+                else:
+                    self.grid.place_agent(SchellingAgent(self, 0), pos)
+        ## Initialize datacollector
         self.datacollector.collect(self)
 
+    ## Define a step: reset global happiness tracker, agents move in random order, collect data
     def step(self):
-        """
-        Run one step of the model.
-        """
-        self.happy = 0  # Reset counter of happy agents
-        self.schedule.step()
-
+        self.happy = 0
+        self.agents.shuffle_do("move")
         self.datacollector.collect(self)
-
-        if self.happy == self.schedule.get_agent_count():
-            self.running = False
+        ## Run model until all agents are happy
+        self.running = self.happy < len(self.agents)
