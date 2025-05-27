a745ffd0e8476922b8f94adcd4c5334110818c4e
jgsbd210945 <jagordon9522@gmail.com>
2025-05-27 18:41:56 -0500

    Final Plotting

--- Final/plotting.R
+++ Final/plotting.R
@@ -1,23 +1,20 @@
 library(tidyverse)
 
-results <- read_csv("results.csv")
-demo_results <- read_csv("dem_results.csv")
+power <- read_csv("power_results.csv")
+dem <- read_csv("dem_results.csv")
+demcon <- read_csv("demcons_results.csv")
 
-results <- results |> rename(consol_lvl = `Consolidation Level`,
-                             dem_lvl = `Democracy Level`)|>
-  mutate(dems = Democracies / num_nodes,
-         autos = Autocracies / num_nodes,
-         greys = Grey / num_nodes)
+clean <- function(df) {
+  df |> rename(consol_lvl = `Consolidation Level`,
+               dem_lvl = `Democracy Level`) |>
+    mutate(dems = Democracies / num_nodes,
+           autos = Autocracies / num_nodes,
+           greys = Grey / num_nodes)
+}
 
-demo_results <- demo_results |> 
-  rename(consol_lvl = `Consolidation Level`,
-         dem_lvl = `Democracy Level`)|>
-  mutate(dems = Democracies / num_nodes,
-         autos = Autocracies / num_nodes,
-         greys = Grey / num_nodes)
-
-
-power_results <- results |> group_by(Step, power_change) |>
+power_results <- power |>
+  clean() |>
+  group_by(Step, power_change) |>
   summarize(dem_lvl = mean(dem_lvl, na.rm = TRUE),
             consol_lvl = mean(consol_lvl, na.rm = TRUE),
             dems = mean(dems),
@@ -29,17 +26,67 @@ power_results |> group_by(power_change) |>
   geom_line() +
   scale_color_grey(name = "Variability in Power") +
   ylab("Democracy Level") +
-  labs(title = "Democracy Level for Increasing Power Variability")
-
+  labs(title = "Democracy Level for Increasing Power Variability",
+       caption = paste("Number of Runs:", length(unique(power$RunId))))
 
 power_results |> group_by(power_change) |>
   ggplot(aes(x = Step, y = consol_lvl, color = factor(power_change))) +
   geom_line() +
   scale_color_grey(name = "Variability in Power") +
   ylab("Consolidation Level") +
-  labs(title = "Consolidation Level for Increasing Power Variability")
+  labs(title = "Consolidation Level for Increasing Power Variability",
+       caption = paste("Number of Runs:", length(unique(power$RunId))))
 
-dem_results <- results |> group_by(Step, dem_levels) |>
+demsimp_results <- demcon |> clean() |>
+  group_by(Step, dem_levels) |>
+  summarize(dem_lvl = mean(dem_lvl, na.rm = TRUE),
+            consol_lvl = mean(consol_lvl, na.rm = TRUE),
+            dems = mean(dems),
+            autos = mean(autos),
+            greys = mean(greys))
+
+demsimp_results |> group_by(dem_levels) |>
+  ggplot(aes(x = Step, y = dem_lvl, color = factor(dem_levels))) +
+  geom_line() +
+  scale_color_grey(name = "Initial Democracies") +
+  ylab("Democracy Level") +
+  labs(title = "Democracy Level Based on Starting Level of Democracies",
+       caption = paste("Number of Runs:", length(unique(demcon$RunId))))
+demsimp_results |> group_by(dem_levels) |>
+  ggplot(aes(x = Step, y = consol_lvl, color = factor(dem_levels))) +
+  geom_line() +
+  scale_color_grey(name = "Initial Democracies") +
+  ylab("Consolidation Level") +
+  labs(title = "Consolidation Level Based on Starting Level of Democracies",
+       caption = paste("Number of Runs:", length(unique(demcon$RunId))))
+
+con_results <- demcon |> clean() |>
+  group_by(Step, consol_levels) |>
+  summarize(dem_lvl = mean(dem_lvl, na.rm = TRUE),
+            consol_lvl = mean(consol_lvl, na.rm = TRUE),
+            dems = mean(dems),
+            autos = mean(autos),
+            greys = mean(greys))
+
+con_results |> group_by(consol_levels) |>
+  ggplot(aes(x = Step, y = dem_lvl, color = factor(consol_levels))) +
+  geom_line() +
+  scale_color_grey(name = "Initial Democracies") +
+  ylab("Democracy Level") +
+  labs(title = "Democracy Level Based on Starting Consolidation Levels",
+       caption = paste("Number of Runs:", length(unique(demcon$RunId))))
+con_results |> group_by(consol_levels) |>
+  ggplot(aes(x = Step, y = consol_lvl, color = factor(consol_levels))) +
+  geom_line() +
+  scale_color_grey(name = "Initial Democracies") +
+  ylab("Consolidation Level") +
+  labs(title = "Consolidation Level Based on Starting Consolidation Levels",
+       caption = paste("Number of Runs:", length(unique(demcon$RunId))))
+
+
+dem_results <- dem |>
+  clean() |>
+  group_by(Step, dem_levels) |>
   summarize(dem_lvl = mean(dem_lvl, na.rm = TRUE),
             consol_lvl = mean(consol_lvl, na.rm = TRUE),
             dems = mean(dems),
@@ -49,38 +96,21 @@ dem_results <- results |> group_by(Step, dem_levels) |>
 dem_results |> group_by(dem_levels) |>
   ggplot(aes(x = Step, y = dem_lvl, color = factor(dem_levels))) +
   geom_line() +
-  scale_color_grey(name = "Initial Democracies") +
+  scale_color_grey() +
+  theme(legend.position = "none") +
   ylab("Democracy Level") +
-  labs(title = "Democracy Level Based on Starting Level of Democracies")
+  labs(title = "Democracy Level Based on Starting Level of Democracies",
+       caption = paste("Number of Runs:", length(unique(dem$RunId))))
 dem_results |> group_by(dem_levels) |>
   ggplot(aes(x = Step, y = consol_lvl, color = factor(dem_levels))) +
   geom_line() +
-  scale_color_grey(name = "Initial Democracies") +
-  ylab("Consolidation Level") +
-  labs(title = "Consolidation Level Based on Starting Level of Democracies")
-
-demo_results <- demo_results |> group_by(Step, dem_levels) |>
-  summarize(dem_lvl = mean(dem_lvl, na.rm = TRUE),
-            consol_lvl = mean(consol_lvl, na.rm = TRUE),
-            dems = mean(dems),
-            autos = mean(autos),
-            greys = mean(greys))
-demo_results |> group_by(dem_levels) |>
-  ggplot(aes(x = Step, y = dem_lvl, color = factor(dem_levels))) +
-  geom_line() +
-  scale_color_grey() +
-  theme(legend.position = "none") +
-  ylab("Democracy Level") +
-  labs(title = "Democracy Level Based on Starting Level of Democracies")
-demo_results |> group_by(dem_levels) |>
-  ggplot(aes(x = Step, y = consol_lvl, color = factor(dem_levels))) +
-  geom_line() +
   scale_color_grey() +
   theme(legend.position = "none") +
   ylab("Consolidation Level") +
-  labs(title = "Consolidation Level Based on Starting Level of Democracies")
+  labs(title = "Consolidation Level Based on Starting Level of Democracies",
+       caption = paste("Number of Runs:", length(unique(dem$RunId))))
 
-gen_results <- results |> group_by(Step) |>
+gen_results <- demcon |> clean() |> group_by(Step) |>
   summarize(dem_lvl = mean(dem_lvl, na.rm = TRUE),
             consol_lvl = mean(consol_lvl, na.rm = TRUE),
             dems = mean(dems),
@@ -91,13 +121,15 @@ gen_results |>
   ggplot(aes(x = Step, y = dem_lvl)) +
   geom_smooth() +
   ylab("Democracy Level") +
-  labs(title = "Mean Democracy Level Across Runs")
+  labs(title = "Mean Democracy Level Across Runs",
+       caption = paste("Number of Runs:", length(unique(demcon$RunId))))
 
 gen_results |>
   ggplot(aes(x = Step, y = consol_lvl)) +
   geom_smooth() +
   ylab("Consolidation Level") +
-  labs(title = "Mean Consolidation Level Across Runs")
+  labs(title = "Mean Consolidation Level Across Runs",
+       caption = paste("Number of Runs:", length(unique(demcon$RunId))))
 
 gen_results |>
   ggplot(aes(x = Step)) +
@@ -107,4 +139,5 @@ gen_results |>
   scale_color_manual(name = "Regime Type",
                      values = c("darkblue", "darkred", "darkgrey")) +
   ylab("Proportion of Regimes") +
-  labs(title = "Average Proportion of States Across Runs")
+  labs(title = "Average Proportion of States Across Runs",
+       caption = paste("Number of Runs:", length(unique(demcon$RunId))))
